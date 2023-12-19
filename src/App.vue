65deb38a6d54bd087598c54ed8ef0e55990f3c34
cosimogiove <cosimo18497@gmail.com>
2023-12-19 11:59:37 +0100

    feat: scaffolding

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Main1 from './Main1.vue';
+import Header1 from './Header1.vue';
+import footer1 from './footer1.vue';
+export default{
+   components:{
+    Main1,
+    Header1,
+    footer1,
+
+   }
+
+}
+
+</script>
+
+<template>
+  <Main1></Main1>
+  <Header1></Header1>
+  <footer1></footer1>
+</template>
+
+<style lang="scss" scoped>
+@use 'style.scss' as *;
+
+
+</style>
---
+++ src/Header1.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+    name:"_header",
+    data(){
+        return{
+            
+        }
+    }
+}
+
+</script>
+
+<template>
+    <header>
+        <div>
+            <div>
+                <img src="" alt="">
+            </div>
+            <div></div>
+        </div>
+    </header>
+  
+</template>
+
+<style scoped>
+
+</style>
